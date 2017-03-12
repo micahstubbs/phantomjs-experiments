b0e048dbd1c685318839ac856600fa958afea59a
Micah Stubbs <micah.stubbs@gmail.com>
2017-03-12 00:38:53 -0800

    kebab case for file names

--- 03-load-speed.js
+++ 03-load-speed.js
@@ -3,7 +3,7 @@ var page = require('webpage').create(),
   t, address;
 
 if (system.args.length === 1) {
-  console.log('Usage: loadspeed.js <some URL>');
+  console.log('Usage: 03-loadspeed.js <some URL>');
   phantom.exit();
 }
 
