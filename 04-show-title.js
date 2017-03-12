343c3f00991465a030c74ec11acd5f1cc0e1a218
Micah Stubbs <micah.stubbs@gmail.com>
2017-03-12 00:45:54 -0800

    add show-title example

---
+++ 04-show-title.js
@@ -0,0 +1,19 @@
+var page = require('webpage').create();
+var system = require('system');
+var address;
+
+if (system.args.length === 1) {
+  console.log('Usage: phantomjs 04-show-title.js <some URL>');
+  phantom.exit();
+}
+
+address = system.args[1];
+page.onConsoleMessage = function(msg) {
+  console.log('Page title is ' + msg);
+};
+page.open(address, function(status) {
+  page.evaluate(function() {
+    console.log(document.title);
+  });
+  phantom.exit();
+});
